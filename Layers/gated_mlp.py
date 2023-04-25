d655e233e8ac308e58304afd9d1a474e48c2f8bb
GH Seol <seollane22@gmail.com>
2023-04-26 05:09:00 +0900

    gated_mlp

---
+++ Layers/gated_mlp.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+def gated_mlp(input, hidden_units):
+    # feedforward network with gating and residual connection
+    dense1 = tf.keras.layers.Dense(hidden_units, activation='relu')(input)
+    gate1 = tf.keras.layers.Dense(hidden_units, activation='sigmoid')(input)
+    gated_dense1 = tf.keras.layers.Multiply()([dense1, gate1])
+    residual1 = tf.keras.layers.Add()([gated_dense1, input])
+
+    # feedforward network with gating and residual connection
+    dense2 = tf.keras.layers.Dense(hidden_units, activation='relu')(residual1)
+    gate2 = tf.keras.layers.Dense(hidden_units, activation='sigmoid')(residual1)
+    gated_dense2 = tf.keras.layers.Multiply()([dense2, gate2])
+    residual2 = tf.keras.layers.Add()([gated_dense2, residual1])
+
+    return residual2
