947c0fc27eb3378f00d7772a8bdf861a49d856c8
GH Seol <seollane22@gmail.com>
2023-05-03 00:53:06 +0900

    Update GD-MLP.py

--- Model/GD-MLP.py
+++ Model/GD-MLP.py
@@ -7,31 +7,31 @@ class GatedDecompositionMLP:
         self.num_features = num_features
         self.num_outputs = num_outputs
         self.hidden_units = hidden_units
-        
-    def gated_residual_mlp(input, hidden_units):
-    # feedforward network with gating and residual connection
-        dense1 = tf.keras.layers.Dense(hidden_units, activation='relu')(input)
-        gate1 = tf.keras.layers.Dense(hidden_units, activation='sigmoid')(input)
+    
+    def gated_residual_mlp(self, input):
+        # feedforward network with gating and residual connection
+        dense1 = tf.keras.layers.Dense(self.hidden_units, activation='relu')(input)
+        gate1 = tf.keras.layers.Dense(self.hidden_units, activation='sigmoid')(input)
         gated_dense1 = tf.keras.layers.Multiply()([dense1, gate1])
         residual1 = tf.keras.layers.Add()([gated_dense1, input])
 
-    # feedforward network with gating and residual connection
-        dense2 = tf.keras.layers.Dense(hidden_units, activation='relu')(residual1)
-        gate2 = tf.keras.layers.Dense(hidden_units, activation='sigmoid')(residual1)
+        # feedforward network with gating and residual connection
+        dense2 = tf.keras.layers.Dense(self.hidden_units, activation='relu')(residual1)
+        gate2 = tf.keras.layers.Dense(self.hidden_units, activation='sigmoid')(residual1)
         gated_dense2 = tf.keras.layers.Multiply()([dense2, gate2])
         residual2 = tf.keras.layers.Add()([gated_dense2, residual1])
 
         return residual2
 
 
-    def time_series_mlp(x_train, y_train, x_test, y_test, num_features, num_outputs, hidden_units):
-    # seasonal decomposition
-        result_train = seasonal_decompose(x_train[:, :, 0], model='additive', period=24)
+    def time_series_mlp(self, x_train, y_train, x_test, y_test):
+        # seasonal decomposition
+        result_train = seasonal_decompose(x_train[:, :, 0], model='additive', period=25)
         trend_train = result_train.trend
         seasonal_train = result_train.seasonal
         residual_train = result_train.resid
 
-        result_test = seasonal_decompose(x_test[:, :, 0], model='additive', period=24)
+        result_test = seasonal_decompose(x_test[:, :, 0], model='additive', period=25)
         trend_test = result_test.trend
         seasonal_test = result_test.seasonal
         residual_test = result_test.resid
@@ -39,16 +39,16 @@ class GatedDecompositionMLP:
         # input layers
         input_trend = tf.keras.layers.Input(shape=(None, 1), name='input_trend')
         input_seasonal = tf.keras.layers.Input(shape=(None, 1), name='input_seasonal')
-        input_residual = tf.keras.layers.Input(shape=(None, num_features-2), name='input_residual')
+        input_residual = tf.keras.layers.Input(shape=(None, self.num_features-2), name='input_residual')
 
         # trend MLP
-        trend_mlp = gated_residual_mlp(input_trend, hidden_units)
+        trend_mlp = self.gated_residual_mlp(input_trend)
 
         # seasonal MLP
-        seasonal_mlp = gated_residual_mlp(input_seasonal, hidden_units)
+        seasonal_mlp = self.gated_residual_mlp(input_seasonal)
 
         # residual MLP
-        residual_mlp = gated_residual_mlp(input_residual, hidden_units)
+        residual_mlp = self.gated_residual_mlp(input_residual)
 
         # combine trend, seasonal, and residual MLPs with weighted sum
         trend_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='trend_weight')(trend_mlp)
