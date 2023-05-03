3dd6ef9ea738ffd589f30df118f5bcb6e8462395
GH Seol <seollane22@gmail.com>
2023-05-03 16:44:59 +0900

    Update GD-MLP.py

--- Model/GD-MLP.py
+++ Model/GD-MLP.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
-from statsmodels.tsa.seasonal import seasonal_decompose
+#from statsmodels.tsa.seasonal import seasonal_decompose
 
 class GatedDecompositionMLP:
     
-    def __init__(self, num_features, num_outputs, hidden_units):
+    def __init__(self, num_features, num_outputs, hidden_units, moving_avg_kernel_size=25, moving_avg_stride=1):
         self.num_features = num_features
         self.num_outputs = num_outputs
         self.hidden_units = hidden_units
+        self.moving_avg_kernel_size = moving_avg_kernel_size
+        self.moving_avg_stride = moving_avg_stride
     
     def gated_residual_mlp(self, input):
         # feedforward network with gating and residual connection
@@ -23,59 +25,52 @@ class GatedDecompositionMLP:
 
         return residual2
 
+    def gd_mlp(self, x_train, y_train, x_test, y_test):
+        # moving average to get the trend
+        moving_average = tf.keras.layers.AveragePooling1D(pool_size=self.moving_avg_kernel_size, strides=self.moving_avg_stride, padding= 'same')
+        
+        # get the trend and residual of train data
+        trend_train = moving_average(x_train)
+        residual_train = x_train - trend_train
 
-    def time_series_mlp(self, x_train, y_train, x_test, y_test):
-        # seasonal decomposition
-        result_train = seasonal_decompose(x_train[:, :, 0], model='additive', period=25)
-        trend_train = result_train.trend
-        seasonal_train = result_train.seasonal
-        residual_train = result_train.resid
+        # get the trend and residual of test data
+        trend_test = moving_average(x_test)
+        residual_test = x_test - trend_test
 
-        result_test = seasonal_decompose(x_test[:, :, 0], model='additive', period=25)
-        trend_test = result_test.trend
-        seasonal_test = result_test.seasonal
-        residual_test = result_test.resid
-
+        print("4:num_feature:{}".format(self.num_features))
         # input layers
-        input_trend = tf.keras.layers.Input(shape=(None, 1), name='input_trend')
-        input_seasonal = tf.keras.layers.Input(shape=(None, 1), name='input_seasonal')
-        input_residual = tf.keras.layers.Input(shape=(None, self.num_features-2), name='input_residual')
+        input_trend = tf.keras.layers.Input(shape=(None, self.num_features), name='input_trend')
+        print("5:trend.shape:{}".format(input_trend.shape))
+        input_residual = tf.keras.layers.Input(shape=(None, self.num_features), name='input_residual')
+
 
         # trend MLP
         trend_mlp = self.gated_residual_mlp(input_trend)
-
-        # seasonal MLP
-        seasonal_mlp = self.gated_residual_mlp(input_seasonal)
-
         # residual MLP
         residual_mlp = self.gated_residual_mlp(input_residual)
-
-        # combine trend, seasonal, and residual MLPs with weighted sum
+        # combine trend and residual MLPs with weighted sum
         trend_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='trend_weight')(trend_mlp)
-        seasonal_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='seasonal_weight')(seasonal_mlp)
         residual_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='residual_weight')(residual_mlp)
 
-        weighted_sum = tf.keras.layers.Concatenate()([
-            trend_mlp * trend_weight,
-            seasonal_mlp * seasonal_weight,
-            residual_mlp * residual_weight,
-            ])
+        weighted_sum =  trend_mlp * trend_weight + residual_mlp * residual_weight
+        print(weighted_sum)
+
         weighted_sum = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=-1))(weighted_sum)
 
         # output layer
         outputs = tf.keras.layers.Dense(self.num_outputs, activation='linear')(weighted_sum)
 
         # define and compile model
-        model = tf.keras.Model(inputs=[input_trend, input_seasonal, input_residual],
+        model = tf.keras.Model(inputs=[input_trend, input_residual],
                               outputs=outputs)
         model.compile(loss='mse', optimizer='adam')
 
         # train model
-        model.fit([trend_train, seasonal_train, residual_train],
+        model.fit([trend_train, residual_train],
                   y_train,
                   epochs=50,
                   batch_size=128,
-                  validation_data=([trend_test, seasonal_test, residual_test], y_test),
+                  validation_data=([trend_test, residual_test], y_test),
                   verbose=2)
 
         return model
