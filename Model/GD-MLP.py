ca1f169802715990019d56d17db6b6233329e810
GH Seol <seollane22@gmail.com>
2023-05-09 00:56:03 +0900

    Update GD-MLP.py

--- Model/GD-MLP.py
+++ Model/GD-MLP.py
@@ -1,75 +1,75 @@
-import tensorflow as tf
-#from statsmodels.tsa.seasonal import seasonal_decompose
-
-class GatedDecompositionMLP:
-    
+class GatedDecompositionMLP(nn.Module):
     def __init__(self, num_features, pred_len, hidden_units, moving_avg_kernel_size=25, moving_avg_stride=1):
+        super(GatedDecompositionMLP, self).__init__()
         self.num_features = num_features
         self.pred_len = pred_len
         self.hidden_units = hidden_units
         self.moving_avg_kernel_size = moving_avg_kernel_size
         self.moving_avg_stride = moving_avg_stride
-    
-    def gated_residual_mlp(self, input):
-        # feedforward network with gating and residual connection
-        dense1 = tf.keras.layers.Dense(self.hidden_units, activation='relu')(input)
-        gate1 = tf.keras.layers.Dense(self.hidden_units, activation='sigmoid')(input)
-        gated_dense1 = tf.keras.layers.Multiply()([dense1, gate1])
-        residual1 = tf.keras.layers.Add()([gated_dense1, input])
 
-        # feedforward network with gating and residual connection
-        dense2 = tf.keras.layers.Dense(self.hidden_units, activation='relu')(residual1)
-        gate2 = tf.keras.layers.Dense(self.hidden_units, activation='sigmoid')(residual1)
-        gated_dense2 = tf.keras.layers.Multiply()([dense2, gate2])
-        residual2 = tf.keras.layers.Add()([gated_dense2, residual1])
+        self.trend_mlp = nn.Sequential(
+            nn.Linear(self.num_features, self.hidden_units),
+            nn.ReLU(),
+            nn.Linear(self.hidden_units, self.hidden_units),
+            nn.ReLU()
+        )
 
-        return residual2
+        self.gate_t = nn.Sigmoid(self.hidden_units, 1) #gate
 
-    def gd_mlp(self, x_train, y_train, x_test, y_test):
+        self.residual_mlp = nn.Sequential(
+            nn.Linear(self.num_features, self.hidden_units),
+            nn.ReLU(),
+            nn.Linear(self.hidden_units, self.hidden_units),
+            nn.ReLU()
+        )
+
+        self.gate_r = nn.Sigmoid(self.hidden_units, 1) #gate
+
+        self.trend_weight = nn.Sequential(
+            nn.Linear(self.hidden_units, 1),
+            nn.Sigmoid()
+        )
+
+        self.residual_weight = nn.Sequential(
+            nn.Linear(self.hidden_units, 1),
+            nn.Sigmoid()
+        )
+
+        self.output_layer = nn.Linear(self.hidden_units, self.pred_len)
+
+    def forward(self, input_trend, input_residual):
         # moving average to get the trend
-        moving_average = tf.keras.layers.AveragePooling1D(pool_size=self.moving_avg_kernel_size, strides=self.moving_avg_stride, padding= 'same')
-        
+        moving_average = nn.AvgPool1d(self.moving_avg_kernel_size, stride=self.moving_avg_stride, padding='same')
+
         # get the trend and residual of train data
-        trend_train = moving_average(x_train)
-        residual_train = x_train - trend_train
+        trend_train = moving_average(input_trend)
+        residual_train = input_trend - trend_train
 
         # get the trend and residual of test data
-        trend_test = moving_average(x_test)
-        residual_test = x_test - trend_test
+        trend_test = moving_average(input_residual)
+        residual_test = input_residual - trend_test
 
-        # input layers
-        input_trend = tf.keras.layers.Input(shape=(None, self.num_features), name='input_trend')
-       
-        input_residual = tf.keras.layers.Input(shape=(None, self.num_features), name='input_residual')
+        # trend MLP (gated)
+        trend_mlp = self.trend_mlp(trend_train)
+        gate_t = self.gate_t(trend_mlp)
+        h1 = nn.Linear(self.hidden_units, self.hidden_units)
+        gate_t = h1.weight.data
+        trend_mlp = h1(trend_mlp)
 
-
-        # trend MLP
-        trend_mlp = self.gated_residual_mlp(input_trend)
-        # residual MLP
-        residual_mlp = self.gated_residual_mlp(input_residual)
+        # residual MLP(gated)
+        residual_mlp = self.residual_mlp(residual_train)
+        gate_r = self.gate_r(residual_mlp)
+        h2 = nn.Linear(self.hidden_units, self.hidden_units)
+        gate_r = h2.weight.data
+        residual_mlp = h2(residual_mlp)
+        
         # combine trend and residual MLPs with weighted sum
-        trend_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='trend_weight')(trend_mlp)
-        residual_weight = tf.keras.layers.Dense(1, activation='sigmoid', name='residual_weight')(residual_mlp)
+        trend_weight = self.trend_weight(trend_mlp)
+        residual_weight = self.residual_weight(residual_mlp)
 
-        weighted_sum =  trend_mlp * trend_weight + residual_mlp * residual_weight
-        print(weighted_sum)
-
-        #weighted_sum = tf.keras.layers.Lambda(lambda x: tf.reduce_sum(x, axis=-1))(weighted_sum)
+        weighted_sum = trend_mlp * trend_weight + residual_mlp * residual_weight
 
         # output layer
-        outputs = tf.keras.layers.Dense(self.pred_len, activation='linear')(weighted_sum)
+        outputs = self.output_layer(weighted_sum)
 
-        # define and compile model
-        model = tf.keras.Model(inputs=[input_trend, input_residual],
-                              outputs=outputs)
-        model.compile(loss='mse', optimizer='adam')
-
-        # train model
-        model.fit([trend_train, residual_train],
-                  y_train,
-                  epochs=50,
-                  batch_size=128,
-                  validation_data=([trend_test, residual_test], y_test),
-                  verbose=2)
-
-        return model
+        return outputs
