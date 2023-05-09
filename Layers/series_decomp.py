ebb91f6fd7fa7fed313425bca88e2bbc896eabbb
GH Seol <seollane22@gmail.com>
2023-05-09 13:12:15 +0900

    upload series_decomp.py

---
+++ Layers/series_decomp.py
@@ -0,0 +1,36 @@
+#same source used in Autoformer, DLinear
+#https://github.com/thuml/Autoformer
+#https://github.com/cure-lab/LTSF-Linear
+
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        trend = self.moving_avg(x)
+        res = x - trend
+        return res, trend
