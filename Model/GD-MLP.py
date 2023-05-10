2aba90fa97fe7448938c4633c6591b19a3d9da56
GH Seol <seollane22@gmail.com>
2023-05-10 13:35:54 +0900

    Update GD-MLP.py

--- Model/GD-MLP.py
+++ Model/GD-MLP.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 class moving_avg(nn.Module):
     """
     Moving average block to highlight the trend of time series
+    * decomposition Source from autoformer, Dlinear
     """
     def __init__(self, kernel_size, stride):
         super(moving_avg, self).__init__()
@@ -27,6 +28,7 @@ class moving_avg(nn.Module):
 class series_decomp(nn.Module):
     """
     Series decomposition block
+    * decomposition Source from autoformer, Dlinear
     """
     def __init__(self, kernel_size):
         super(series_decomp, self).__init__()
@@ -46,18 +48,18 @@ class gated_mlp (nn.Module):
     """
     def __init__(self, num_features, pred_len, hidden_units):
         super(gated_mlp, self).__init__()
-        self.num_features = num_features
-        self.pred_len = pred_len
-        self.hidden_units = hidden_units
-        kernel_size = 25
+        self.num_features = num_features #피처 수
+        self.pred_len = pred_len # 예측 길이
+        self.hidden_units = hidden_units #노드 수
+        kernel_size = 25 #same as Autoformer, Dlinear
         self.decomposition = series_decomp(kernel_size)
 
-        self.trend_mlp = nn.Sequential(
-            nn.Linear(self.num_features, self.hidden_units),
+        self.trend_mlp = nn.Sequential( 
+            nn.Linear(self.num_features, self.hidden_units), 
             nn.ReLU(),
             nn.Linear(self.hidden_units, self.hidden_units),
             nn.ReLU()
-        )
+        ) # MLP for Trend
 
 
         self.residual_mlp = nn.Sequential(
@@ -65,11 +67,11 @@ class gated_mlp (nn.Module):
             nn.ReLU(),
             nn.Linear(self.hidden_units, self.hidden_units),
             nn.ReLU()
-        )
+        ) # MLP for Residual
 
         self.gate = nn.Sequential(
             nn.Linear(self.hidden_units, self.hidden_units),
-            nn.Sigmoid()) #gate
+            nn.Sigmoid()) #gate for both Trend and Residual
 
         self.layer = nn.Linear(self.hidden_units, self.hidden_units)
 
@@ -79,16 +81,16 @@ class gated_mlp (nn.Module):
         residual_train,trend_train = self.decomposition(x)
 
         # trend MLP (gated)
-        trend_mlp = self.trend_mlp(trend_train)
-        gate_t = self.gate(trend_mlp)
-        trend_mlp = trend_mlp * gate_t
+        trend_mlp = self.trend_mlp(trend_train) #MLP 통과
+        gate_t = self.gate(trend_mlp) #gate 통과
+        trend_mlp = trend_mlp * gate_t #element-wise product #torch.mul(residual_mlp, gate_r)
         trend_mlp = self.layer(trend_mlp)
         
 
         # residual MLP(gated)
-        residual_mlp = self.residual_mlp(residual_train)
-        gate_r = self.gate(residual_mlp)
-        residual_mlp = residual_mlp * gate_r
+        residual_mlp = self.residual_mlp(residual_train) #MLP 적용
+        gate_r = self.gate(residual_mlp) #gate 통과
+        residual_mlp = residual_mlp * gate_r #element-wise product #torch.mul(residual_mlp, gate_r)
         residual_mlp = self.layer(residual_mlp)
 
         return trend_mlp, residual_mlp
@@ -103,34 +105,39 @@ class gated_sum (nn.Module):
      
     def __init__(self, num_features, pred_len, hidden_units):
         super(gated_sum, self).__init__()
-        self.num_features = num_features
+        self.num_features = num_features #same as gated mlp
         self.pred_len = pred_len
         self.hidden_units = hidden_units
         kernel_size = 25
         self.decomposition = series_decomp(kernel_size)
-        self.gated_mlp = gated_mlp(num_features, pred_len, hidden_units) # 부모 클래스의 상속받은 인스턴스 다시 쓰려면 초기화해줘야 한다.
-
+        self.gated_mlp = gated_mlp(num_features, pred_len, hidden_units) # 상속받은 부모 클래스의 인스턴스의 속성을 다시 쓰기 위해 초기화해줘야 한다.
+        
+        self.output_layer = nn.Linear(self.hidden_units,self.pred_len) #pred_len으로 산출하기 위한 output_layer
+        
         self.trend_weight = nn.Sequential(
-            nn.Linear(self.hidden_units, 1), #가중합 하기 위한 gate
-            nn.Sigmoid()
+            nn.Linear(self.hidden_units, 1), #가중합 하기 위한 gate  #1은 A/B testing(A:1, B:pred_len) 예정
+            nn.Sigmoid() 
         )
 
         self.residual_weight = nn.Sequential(
-            nn.Linear(self.hidden_units, 1),  #가중합 하기 위한 gate
+            nn.Linear(self.hidden_units, 1),  #가중합 하기 위한 gate  #1은 A/B testing(A:1, B:pred_len) 예정
             nn.Sigmoid()
         )
         
-        self.output_layer = nn.Linear(self.hidden_units,self.pred_len) #실험 예정
 
     def forward(self, x):
-        trend_mlp, residual_mlp = self.gated_mlp(x)  
-        # combine trend and residual MLPs with weighted sum
-        trend_weight = self.trend_weight(trend_mlp)
-        residual_weight = self.residual_weight(residual_mlp)
-
-        weighted_sum = trend_mlp * trend_weight + residual_mlp * residual_weight
-
+        
+        # gated_mlp
+        trend_mlp, residual_mlp = self.gated_mlp(x) 
+        
         # output layer
-        outputs = self.output_layer(weighted_sum)
+        output_trend = self.output_layer(trend_mlp)
+        output_residual = self.output_layer(residual_mlp)
+        
+        # combine trend and residual MLPs with weighted sum
+        trend_weight = self.trend_weight(output_trend)
+        residual_weight = self.residual_weight(outpur_residual)
 
-        return outputs
+        weighted_sum = (trend_mlp * trend_weight) + (residual_mlp * residual_weight)
+
+        return weighted_sum
