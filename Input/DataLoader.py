f3c9779c05c249a84922bd1e96ae2722bfb17890
GH Seol <seollane22@gmail.com>
2023-05-15 13:08:53 +0900

    DataLoader.py

---
+++ Input/DataLoader.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+batch_size=8 
+
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+train_features, train_labels = next(iter(train_loader))
+test_features, test_labels = next(iter(test_loader))
+
+#check it
+print(train_labels.shape)
+print(train_features.shape)
+print(test_labels.shape)
+print(test_features.shape)
