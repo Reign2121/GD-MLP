48d4fe8dc43c0a4f128763e246b1b864031db328
GH Seol <seollane22@gmail.com>
2023-05-15 13:10:52 +0900

    Update Dataset.py

--- Input/Dataset.py
+++ Input/Dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 class Custom_Seq_Dataset(Dataset):
     def __init__(self, data, input_len,label_len, pred_len, target):
         self.data = data
-        self.target = data[target].values #넘파이로 넣어야 한대
+        self.target = data[target].values #넘파이
         self.data = data.values
         self.input_len = input_len
         self.label_len = label_len
@@ -24,11 +24,25 @@ class Custom_Seq_Dataset(Dataset):
         # convert to tensor and reshape
         feature = torch.tensor(feature, dtype=torch.float32)
         target = torch.tensor(target, dtype=torch.float32)
-        #feature = torch.tensor(feature, dtype=torch.float32).unsqueeze(0) #이렇게 하니까 차원이 하나 더 생기네...
-        target = torch.tensor(target, dtype=torch.float32).unsqueeze(1) #unsqueeze, 차원확장
+        target = torch.tensor(target, dtype=torch.float32).unsqueeze(1) #unsqueeze, 차원확장 (feature 차원 추가)
         
         return feature, target
       
       data = Custom_Seq_Dataset(data=data,input_len=336, label_len=96,pred_len=96,target='OT') #Hyperparameter
+    
+    #분할
+    
+train_size = int(0.7 * len(data))  #분할했을 때 수 반환해서 미리 체크
+val_size = int(0.1 * len(data))    
+test_size = len(data) - train_size - val_size
+
+train_indices = list(range(train_size)) #데이터를 시간의 순서대로 분할하기 위해 순서대로 인덱스 생성
+val_indices = list(range(train_size, train_size + val_size))
+test_indices = list(range(train_size + val_size, len(data)))
+
+from torch.utils.data import Subset #하위 데이터로 나누기.
+train_set = Subset(data, train_indices) #(데이터 셋, 인덱스).
+val_set = Subset(data, val_indices)
+test_set = Subset(data, test_indices)
       
       
