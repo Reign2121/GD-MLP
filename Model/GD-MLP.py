b5ee125e1a824ad512191b05f780b18fbe791ef5
GH Seol <seollane22@gmail.com>
2023-05-28 23:01:22 +0900

    Update GD-MLP.py

--- Model/GD-MLP.py
+++ Model/GD-MLP.py
@@ -63,18 +63,14 @@ class gated_mlp (nn.Module):
 
 
         self.trend_mlp = nn.Sequential(
-            nn.Linear(self.seq_len, self.hidden_units), 
-            #nn.ReLU(),
-            nn.Linear(self.hidden_units, self.hidden_units),
-            #nn.ReLU()
+            nn.Linear(self.seq_len, self.hidden_units),
+            nn.Linear(self.hidden_units, self.hidden_units)
         ) # MLP for Trend
 
 
         self.residual_mlp = nn.Sequential(
             nn.Linear(self.seq_len, self.hidden_units),
-            #nn.ReLU(),
-            nn.Linear(self.hidden_units, self.hidden_units),
-            #nn.ReLU()
+            nn.Linear(self.hidden_units, self.hidden_units)
         ) # MLP for Residual
 
 
