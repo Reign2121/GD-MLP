cf18d6fbe42c7499e80b70f798156974028e1fa4
GH Seol <seollane22@gmail.com>
2023-08-03 13:19:51 +0900

    Update gated_mlp.py

--- Layers/gated_mlp.py
+++ Layers/gated_mlp.py
@@ -39,17 +39,17 @@ class gated_mlp (nn.Module):
         
         residual_train,trend_train = self.decomposition(x) # torch.Size([8, 336, 1])
 
-        #input_layer 
+        #input_layer #not necessary
         trend_train = self.input_layer(trend_train) #-> torch.Size([8, 336, 1])
         residual_train = self.input_layer(residual_train)
 
         residual_train,trend_train = residual_train.permute(0,2,1), trend_train.permute(0,2,1) #-> torch.Size([8, 1, 336])
-
+        
         #input_gate
         i_gate_t = self.input_gate(trend_train) #-> torch.Size([8, 1, 336])
         trend_train = trend_train * i_gate_t #-> torch.Size([8, 1, 336])
 
-        i_gate_r = (1 - self.input_gate(trend_train)) #-> torch.Size([8, 1, 336])
+        i_gate_r = self.input_gate(residual_train) #-> torch.Size([8, 1, 336])
         residual_train = residual_train * i_gate_r #-> torch.Size([8, 1, 336])
 
 
