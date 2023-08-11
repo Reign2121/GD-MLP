e35a93e23416c188877f800550b375d765717f2d
GH Seol <seollane22@gmail.com>
2023-08-11 15:17:48 +0900

    Update gated_sum(trend+residual).py

--- Layers/gated_sum(trend+residual).py
+++ Layers/gated_sum(trend+residual).py
@@ -10,23 +10,22 @@ class gated_sum (nn.Module):
      
     def __init__(self, seq_len, num_features, pred_len, hidden_units):
         super(gated_sum, self).__init__()
-        self.seq_len = seq_len #인풋 길이
+        self.seq_len = seq_len #인풋 길이 #ex) 336
         self.num_features = num_features #same as gated mlp
-        self.pred_len = pred_len
-        self.hidden_units = hidden_units
+        self.pred_len = pred_len #ex) 96
+        self.hidden_units = hidden_units #ex) 512
         kernel_size = 25
         self.decomposition = series_decomp(kernel_size)
         self.gated_mlp = gated_mlp(seq_len, num_features, pred_len, hidden_units) 
         
-        self.output_layer = nn.Linear(self.hidden_units,self.pred_len) #pred_len으로 산출하기 위한 output_layer
+        self.output_layer = nn.Linear(self.hidden_units,self.pred_len) #pred_len으로 선형변환하기 위한 output_layer
         
         #Output gate
         self.output_gate = nn.Sequential(
             nn.Linear(self.pred_len, 1), #가중합 하기 위한 gate  
             nn.Sigmoid())
 
-
-
+    
     def forward(self, x):
         
         # gated_mlp
@@ -38,10 +37,9 @@ class gated_sum (nn.Module):
         
         # combine trend and residual MLPs with weighted sum
         trend_weight = self.output_gate(output_trend) # gate 통과 #->torch.Size([8, 1, 1])
-        residual_weight = (1 - trend_weight)
+        residual_weight = (1-trend_weight)
 
         #trend_weight,residual_weight = trend_weight.permute(0,2,1), residual_weight.permute(0,2,1)
-
         weighted_sum = (output_trend * trend_weight) + (output_residual * residual_weight) # Weighted sum == Final Output #->torch.Size([8, 1, 96])
         
         return weighted_sum.permute(0,2,1) #Final Output #->torch.Size([8, 96, 1])
